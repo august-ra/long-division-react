9b0405104e7d8b0f443ec86e8e448e909e66f5e3
Anton Voytov <august-ra@ya.ru>
2025-03-06 01:09:05 +0300

    Added functions

---
+++ src/utils/info.ts
@@ -0,0 +1,23 @@
+import { emptyLastStep } from "./types"
+import type { NumberInfo, StepInfo } from "./types"
+
+
+export function fillStepInfo(info: StepInfo, first: number, zeros: number, second: number, quotient: number, output: number) {
+  setNumberInfo(info.first, first, zeros)
+  setNumberInfo(info.second!, second)
+  info.quotient = quotient
+  info.output   = output
+}
+
+export function getLastStepInfo(first: number): StepInfo {
+  const info: StepInfo = { ...emptyLastStep }
+  setNumberInfo(info.first, first)
+
+  return info
+}
+
+export function setNumberInfo(info: NumberInfo, value: number, zeros: number = 0) {
+  info.value = value
+  info.zeros = zeros
+  info.count = String(value).length
+}
