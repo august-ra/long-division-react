f3d23ce149b492915d45107de07d4cdce41ea2d3
Anton Voytov <august-ra@ya.ru>
2025-03-06 00:51:36 +0300

    Added inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import React, { useState } from "react"
 import "./App.css"
 
 
 export default function App() {
+  const [dividend, setDividend] = useState<number>(1)
+  const [divisor,  setDivisor]  = useState<number>(589)
+
+  function changeNumber(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.name, "::", event.target.value)
+
+    if (event.target.name === "dividend")
+      setDividend(Number(event.target.value))
+    else
+      setDivisor(Number(event.target.value))
+  }
+
   return (
-    <div>
-      test
-    </div>
+    <>
+      <div className="start">
+        Введите целое делимое <input type="number" name="dividend" value={dividend} onInput={changeNumber} /> и целый делитель <input type="number" name="divisor" value={divisor} onInput={changeNumber} /> для показа деления в столбик.
+      </div>
+
+      <div className="empty" />
+    </>
   )
 }
